edb816945d1ad36f395644f7ae7ad9e2d0d39b27
Jeff Reiher <jreiher2003@yahoo.com>
2017-02-15 14:22:14 +0000

    Add the other pages

--- app/views.py
+++ app/views.py
@@ -5,3 +5,15 @@ from flask import render_template, url_for
 def index():
     return render_template("index.html")
 
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/team")
+def team():
+    return render_template("team.html")
+
